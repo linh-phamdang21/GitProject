884a0ae91693df724d535270d5b1ab59076106f2
linh-phamdang21 <ph.danglinh214@gmail.com>
2020-02-19 14:57:12 +0700

    Add Exercise

---
+++ myjs2.js
@@ -0,0 +1,5 @@
+let inputWidth = parseInt(prompt("Enter the width"));
+let inputHeight = parseInt(prompt("Enter the height"));
+let area = inputHeight * inputWidth;
+document.write("Width = " + inputWidth + "<br>" + "Height = " + inputHeight + "<br>")
+document.write("Area = " + area)
